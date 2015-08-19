ba257758af3a420225bc14268a6d414f5948a96b
Piotr Skamruk <piotr.skamruk@intel.com>
2015-08-19 14:28:50 +0200

    kvm: Entering into pod

--- rkt/run_prepared.go
+++ rkt/run_prepared.go
@@ -144,6 +144,6 @@ func runRunPrepared(cmd *cobra.Command, args []string) (exit int) {
 		MDSRegister: flagMDSRegister,
 		Apps:        apps,
 	}
-	stage0.Run(rcfg, p.path()) // execs, never returns
+	stage0.Run(rcfg, p.path(), globalFlags.Dir) // execs, never returns
 	return 1
 }
--- stage1/usr_from_kvm/usr_from_kvm.mk
+++ stage1/usr_from_kvm/usr_from_kvm.mk
@@ -2,10 +2,12 @@ UFK_STAMPS :=
 UFK_INCLUDES := \
 	../usr_from_coreos/usr_from_coreos.mk \
 	kernel.mk \
+	files.mk \
 	lkvm.mk
 $(call setup-stamp-file,UFK_REPLACE_FLAVOR_STAMP)
 $(call setup-tmp-dir,UFK_TMPDIR)
 
+UFC_MANIFESTS_DIR := $(MK_SRCDIR)/manifest.d
 $(call inc-many,$(UFK_INCLUDES))
 
 STAGE1_STAMPS += $(UFK_REPLACE_FLAVOR_STAMP)
@@ -16,6 +18,7 @@ $(call forward-vars,$(UFK_REPLACE_FLAVOR_STAMP), \
 $(UFK_REPLACE_FLAVOR_STAMP):
 	rm -f "$(ACIROOTFSDIR)/flavor"
 	ln -sf 'kvm' "$(ACIROOTFSDIR)/flavor"
+	ln -sf 'enter_kvm' "$(ACIROOTFSDIR)/enter"
 	touch "$@"
 
 $(call undefine-namespaces,UFK)
