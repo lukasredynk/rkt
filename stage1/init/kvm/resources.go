245541f98eccc312f296492c75a74ecadd580c79
mstachowski <michal.stachowski@outlook.com>
2015-12-16 13:43:35 -0600

    kvm: Add support for capabilities limitation
    Cherry pick for tests purposses. It should be merged in
    pull request #2222
    
    By default kvm flavor has got enabled every capability inside pod.
    This is normal systemd behavior, when its running as a init process
    inside VM. Coreos pod is started by systemd-nspawn, which apply some
    initial capabilities by default.
    
    This patch add support for restricted set of capabilities inside rkt-kvm
    pod

--- stage1/init/kvm/resources.go
+++ stage1/init/kvm/resources.go
@@ -66,3 +66,19 @@ func GetAppsResources(apps schema.AppList) (totalCpus, totalMem int64) {
 
 	return totalCpus, totalMem
 }
+
+// GetAppCapabilities is a filter which returns list of valid Linux capabilities
+// Function requires a manifest of application
+func GetAppCapabilities(app *types.App) []string {
+
+	var caps []string
+	for _, i := range app.Isolators {
+		switch v := i.Value().(type) {
+		case types.LinuxCapabilitiesSet:
+			for _, c := range v.Set() {
+				caps = append(caps, string(c))
+			}
+		}
+	}
+	return caps
+}
